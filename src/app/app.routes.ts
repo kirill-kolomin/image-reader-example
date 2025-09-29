69c601f1670a985397dcd2acdcc23dbe665de778
kirill-kolomin <kkolomin.w@gmail.com>
2025-09-29 19:56:08 +0300

    Apply routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DocumentViewerComponent} from './pages/document-viewer/document-viewer.component';
+import {ResourceNotFound} from './pages/resource-not-found/resource-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'documents/:id', component: DocumentViewerComponent},
+  {path: '**', component: ResourceNotFound}
+];
