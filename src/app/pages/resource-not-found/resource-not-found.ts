778b92c668354ec3c9b52590854790d4b47e6103
kirill-kolomin <kkolomin.w@gmail.com>
2025-09-29 19:54:28 +0300

    Add 404 component

---
+++ src/app/pages/resource-not-found/resource-not-found.ts
@@ -0,0 +1,10 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-resource-not-found',
+  imports: [],
+  templateUrl: './resource-not-found.html',
+  styleUrl: './resource-not-found.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ResourceNotFound {}
