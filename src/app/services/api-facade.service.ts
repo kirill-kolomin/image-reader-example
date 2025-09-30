1f46ea165a7102ed02b190af15f98815928b0c94
kirill-kolomin <kkolomin.w@gmail.com>
2025-09-30 06:03:10 +0300

    Add get page method

--- src/app/services/api-facade.service.ts
+++ src/app/services/api-facade.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {catchError, Observable, of} from 'rxjs';
+import {catchError, Observable, of, switchMap, throwError} from 'rxjs';
 import {Document} from '../models/document.model';
 import mock from '../../mocks/1.json';
 
@@ -18,5 +18,21 @@ export class ApiFacadeService {
           return of({id: crypto.randomUUID(), ...mock})
         })
       );
+  }
+
+  // TODO: Decorate for showing error toasts
+  getPage(pageUrl: string): Observable<string> {
+    return this.#httpClient.get(`${pageUrl}`, {observe: 'response', responseType: 'blob'})
+      .pipe(
+        switchMap(response => {
+          if (!response.body) {
+            return throwError(() => new Error('No content'));
+          }
+
+          const objectURL = URL.createObjectURL(response.body);
+
+          return of(objectURL)
+        })
+      )
   }
 }
