64f663fb7bdddb61ff829591de2fda31552b7a27
kirill-kolomin <kkolomin.w@gmail.com>
2025-09-30 06:03:48 +0300

    Add document viewer component

---
+++ src/app/pages/document-viewer/document-viewer.component.ts
@@ -0,0 +1,67 @@
+import {ChangeDetectionStrategy, Component, inject, OnInit, Signal} from '@angular/core';
+import {DocumentViewerService} from './services/document-viewer.service';
+import {Observable} from 'rxjs';
+import {DocumentWithAnnotations, Page} from '../../models/document.model';
+import {AnnotationComponent} from '../../components/annotation/annotation.component';
+import {AnnotationsForPagePipe} from '../../pipes/annotations-for-page.pipe';
+import {AsyncPipe, JsonPipe, PercentPipe} from '@angular/common';
+import {AnnotationsService} from './services/annotations.service';
+import {ZoomService} from './services/zoom.service';
+
+@Component({
+  selector: 'app-document-viewer',
+  imports: [AnnotationComponent, AnnotationsForPagePipe, AsyncPipe, PercentPipe, JsonPipe],
+  templateUrl: './document-viewer.component.html',
+  styleUrls: ['./document-viewer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [DocumentViewerService, AnnotationsService, ZoomService],
+})
+export class DocumentViewerComponent implements OnInit {
+  document: Signal<DocumentWithAnnotations | null>;
+  pages: Signal<string[] | null>;
+
+  zoomLevel$: Observable<number>;
+
+  #documentViewerService =  inject(DocumentViewerService);
+  #zoomService =  inject(ZoomService);
+  #annotationsService =  inject(AnnotationsService);
+
+  constructor(
+  ) {
+    this.document = this.#documentViewerService.document;
+    this.pages = this.#documentViewerService.pages;
+    this.zoomLevel$ = this.#zoomService.zoomLevel$;
+  }
+
+  ngOnInit() {
+    this.#documentViewerService.run();
+  }
+
+  increaseZoom(): void {
+    this.#zoomService.increaseZoom();
+  }
+
+  decreaseZoom(): void {
+    this.#zoomService.decreaseZoom();
+  }
+
+  saveDocument(): void {
+    this.#documentViewerService.saveDocument();
+  }
+
+  addAnnotation(event: MouseEvent, page: Page): void {
+    const rect = (event.target as HTMLElement).getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    const text = prompt('Enter annotation text:');
+    if (text) {
+      this.#annotationsService.addAnnotation({
+        pageId: page.number,
+        text,
+        x,
+        y
+      });
+    }
+  }
+}
