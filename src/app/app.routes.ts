c52212423f53292e13fdd27c9d937d35668f2975
kirill-kolomin <kkolomin.w@gmail.com>
2025-09-30 05:13:13 +0300

    Use variable for route param

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import {Routes} from '@angular/router';
 import {DocumentViewerComponent} from './pages/document-viewer/document-viewer.component';
 import {ResourceNotFound} from './pages/resource-not-found/resource-not-found';
+import {DOCUMENT_ID} from './models/route-params';
 
 export const routes: Routes = [
-  {path: 'documents/:id', component: DocumentViewerComponent},
+  {path: `documents/:${DOCUMENT_ID}`, component: DocumentViewerComponent},
   {path: '**', component: ResourceNotFound}
 ];
